6a95912fe34c36004773bb91b9b3c64afac84ada
ATUL VERMA <atulverma.btech74@gmail.com>
2025-02-01 20:00:30 +0530

    feature/User-module  Added User Module

---
+++ src/common/entity/Base.entity.ts
@@ -0,0 +1,30 @@
+import { PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, Column, BeforeInsert } from 'typeorm';
+
+
+export abstract class BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+    updatedAt: Date;
+
+    @Column({ type: 'int', nullable: true })
+    createdBy: number; 
+
+    @Column({ type: 'int', nullable: true })
+    updatedBy: number;  
+
+    @BeforeInsert()
+    setCreatedBy() {
+        
+        this.createdBy = 1;
+    }
+
+    @BeforeInsert()
+    setUpdatedBy() {
+        this.updatedBy = 1;
+    }
+}
