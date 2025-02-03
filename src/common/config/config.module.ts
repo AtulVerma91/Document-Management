982add0992a17b33a528db9255c3f868f61f5592
ATUL VERMA <atulverma.btech74@gmail.com>
2025-02-03 18:02:58 +0530

    bugfix/migration

--- src/common/config/config.module.ts
+++ src/common/config/config.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from './config.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ORM_CONFIGS } from './orm-config';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        ...ORM_CONFIGS(configService),
+        autoLoadEntities: true,
+        migrationsRun: true,
+      }),
+      dataSourceFactory: async (options) => {
+        const dataSource = new DataSource(options);
+        return dataSource.initialize();
+      },
+    }),],
   providers: [ConfigService],
   exports: [ConfigService],
 })
--- src/common/config/config.service.ts
+++ src/common/config/config.service.ts
@@ -5,11 +5,18 @@ export const config = () => ({
     logLevel: process.env.OMS_LOG_LEVEL,
   },
   postgres: {
-    host: process.env.POSTGRES_HOST,
-    port: +process.env.POSTGRES_PORT,
-    username: process.env[`POSTGRES_USER`],
-    password: process.env[`POSTGRES_PASSWORD`],
-    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: +process.env.POSTGRES_PORT || 5432,
+    username: process.env[`POSTGRES_USER`]||'admin',
+    password: process.env[`POSTGRES_PASSWORD`] ||'adminpassword',
+    database: process.env[`POSTGRES_DB`]||'app_db',
+    entities: [
+      __dirname + '/../**/*.entity{.ts,.js}',
+      __dirname + '/../entity/*.entity{.ts,.js}',
+    ],
+    logging: false,
+    synchronize: false,
+    migrations: ['dist/migrations/*.js'],
   },
   jwtSecret: process.env[`JWT_SECRET`] || 'secret',
 });
